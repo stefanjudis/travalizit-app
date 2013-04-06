d9d13d130d5418a1a779a851aef90d65c4faaab6
stefan judis <stefanjudis@gmail.com>
2013-04-06 22:24:56 +0200

    coding style gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,57 +2,58 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    compass: {                  // Task
-      dist: {                   // Target
+    compass : {                  // Task
+      dist : {                   // Target
         options: {              // Target options
-          sassDir: 'lib/sass',
-          cssDir: 'assets/css',
-          environment: 'production'
+          sassDir     : 'lib/sass',
+          cssDir      : 'assets/css',
+          environment : 'production',
+          imagesDir   : 'assets/css'
         }
       },
-      dev: {                    // Another target
-        options: {
-          sassDir: 'sass',
-          cssDir: 'css'
+      dev : {                    // Another target
+        options : {
+          sassDir : 'sass',
+          cssDir  : 'css'
         }
       }
     },
 
-    pkg: grunt.file.readJSON('package.json'),
+    pkg: grunt.file.readJSON( 'package.json' ),
 
     // nodeunit: {
     //   all: ['test/**/*Test.js']
     // },
     //
-    jshint: {
-      all: ['Gruntfile.js', 'lib/**/*.js', 'test/**/*.js'],
-      options: {
-        jshintrc: '.jshintrc'
+    jshint : {
+      all : [ 'Gruntfile.js', 'lib/**/*.js', 'test/**/*.js' ],
+      options : {
+        jshintrc : '.jshintrc'
       }
     },
 
-    watch: {
-      gruntfile: {
-        files: 'Gruntfile.js',
-        tasks: ['jshint:gruntfile'],
-        options: {
-          nocase: true
+    watch : {
+      gruntfile : {
+        files   : 'Gruntfile.js',
+        tasks   : [ 'jshint:gruntfile' ],
+        options : {
+          nocase : true
         }
       },
-      src: {
-        files: ['lib/**/*', 'test/**/*.js', 'app.js', 'Gruntfile.js'],
-        tasks: ['default']
+      src : {
+        files : [ 'lib/**/*', 'test/**/*.js', 'app.js', 'Gruntfile.js' ],
+        tasks : [ 'default' ]
       }
     }
   });
 
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  //grunt.loadNpmTasks('grunt-contrib-nodeunit');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+  //grunt.loadNpmTasks( 'grunt-contrib-nodeunit' );
+  grunt.loadNpmTasks( 'grunt-contrib-watch' );
+  grunt.loadNpmTasks( 'grunt-contrib-compass' );
 
   // Default task.
-  grunt.registerTask('default', ['jshint', 'compass']);
+  grunt.registerTask( 'default', [ 'jshint', 'compass' ] );
   //grunt.registerTask('travis', ['jshint', 'nodeunit']);
 };
