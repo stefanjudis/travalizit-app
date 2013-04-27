73061c5e4bda2f9b42075eb8fa85b289cb640b0c
stefan judis <stefanjudis@gmail.com>
2013-04-27 14:08:19 +0200

    added new params config structure

--- assets/js/config/config.js
+++ assets/js/config/config.js
@@ -2,57 +2,70 @@ define([], function() {
   return {
     charts : [
       {
-        icon   : 'wrench',
-        name   : 'builds',
+        icon   : 'dashboard',
+        name   : 'Builds per Time',
         params : [
-          'number',
-          'status',
-          'started_at',
-          'finished_at',
-          'repository_id',
-          'agent',
-          'create_at',
-          'updated_at',
-          'config',
-          'commit_id',
-          'request_id',
-          'state',
-          'language',
-          'archived_at',
-          'duration',
-          'owner_id',
-          'owner_type',
-          'result',
-          'previous_result',
-          'event_type'
-        ],
-        srcPath : '/builds'
-      }, {
-        icon : 'bookmark-empty',
-        name : 'commits'
-
-      }, {
-        icon : 'th-large',
-        name : 'dimensions'
-      }, {
-        icon : 'suitcase',
-        name : 'jobs'
-      }, {
-        icon : 'group',
-        name : 'memberships'
-      }, {
-        icon : 'building',
-        name : 'organizations'
-      }, {
-        icon : 'hdd',
-        name : 'repositories'
-      }, {
-        icon : 'phone-sign',
-        name : 'requests'
-      }, {
-        icon : 'signout',
-        name : 'urls'
+          {
+            name : 'startDate',
+            type : 'date'
+          }, {
+            name : 'endDate',
+            type : 'date'
+          }
+        ]
       }
+      // {
+      //   icon   : 'wrench',
+      //   name   : 'builds',
+      //   params : [
+      //     'number',
+      //     'status',
+      //     'started_at',
+      //     'finished_at',
+      //     'repository_id',
+      //     'agent',
+      //     'create_at',
+      //     'updated_at',
+      //     'config',
+      //     'commit_id',
+      //     'request_id',
+      //     'state',
+      //     'language',
+      //     'archived_at',
+      //     'duration',
+      //     'owner_id',
+      //     'owner_type',
+      //     'result',
+      //     'previous_result',
+      //     'event_type'
+      //   ],
+      //   srcPath : '/builds'
+      // }, {
+      //   icon : 'bookmark-empty',
+      //   name : 'commits'
+
+      // }, {
+      //   icon : 'th-large',
+      //   name : 'dimensions'
+      // }, {
+      //   icon : 'suitcase',
+      //   name : 'jobs'
+      // }, {
+      //   icon : 'group',
+      //   name : 'memberships'
+      // }, {
+      //   icon : 'building',
+      //   name : 'organizations'
+      // }, {
+      //   icon : 'hdd',
+      //   name : 'repositories'
+      // }, {
+      //   icon : 'phone-sign',
+      //   name : 'requests'
+      // }, {
+      //   icon : 'signout',
+      //   name : 'urls'
+      // }
     ]
   };
 });
